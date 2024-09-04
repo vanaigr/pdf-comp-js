1cf1fb84994dda43e9d8449cf1aa72e44452575d
vanaigr <vanaigranov@gmail.com>
2024-09-04 10:56:22 -0500

    start working on jspdf table

--- back-src/index.js
+++ back-src/index.js
@@ -1,11 +1,16 @@
 import express from 'express'
 import PDFDocument from 'pdfkit'
 import { jsPDF } from 'jspdf'
+import _autoTable from 'jspdf-autotable'
+const autoTable = _autoTable['default']
 
 const app = express()
 
 // https://pdfkit.org/docs/getting_started.html
 app.get('/pdfkit-doc.pdf', (q, s) => {
+    s.status(404)
+    s.send()
+    return
     s.setHeader('Content-Type', 'application/pdf')
 
     const w = 850, h = 1100
@@ -28,15 +33,76 @@ app.get('/pdfkit-doc.pdf', (q, s) => {
     doc.end()
 })
 
+// last commit - last year
+
+// jspdf conversion factors:
+// https://github.com/parallax/jsPDF/blob/ddbfc0f0250ca908f8061a72fa057116b7613e78/jspdf.js#L791
+
 // https://artskydj.github.io/jsPDF/docs/jsPDF.html
 app.get('/jspdf-doc.pdf', (q, s) => {
-    const w = 850, h = 1100
+    const inchToPx = 25.4
+    const w = 8.5 * inchToPx, h = 11 * inchToPx
+    const ptToUnit = inchToPx / 72
 
     const start = performance.now()
 
     s.setHeader('Content-Type', 'application/pdf')
-    const doc = new jsPDF({ unit: 'px', format: [w, h], putOnlyUsedFonts: true })
-    doc.text('document', w * 0.5, 10, { align: 'center', baseline: 'top' })
+    const doc = new jsPDF({ unit: 'mm', format: 'letter', putOnlyUsedFonts: true })
+    doc.setFont('times')
+    doc.setFontSize(20 * ptToUnit)
+
+    // https://artskydj.github.io/jsPDF/docs/module-split_text_to_size.html#~splitTextToSize
+    // Undocumented options. lineHeightFactor doesn't work
+    const dim = doc.getTextDimensions(
+        ('document').repeat(100),
+        {
+            align: 'center',
+            baseline: 'top',
+            maxWidth: w - 4 * inchToPx,
+        }
+    )
+
+    doc.text(
+        ('document').repeat(100),
+        w * 0.5, 10,
+        {
+            align: 'center',
+            baseline: 'top',
+            maxWidth: w - 4 * inchToPx,
+        }
+    )
+
+    doc.setFont('times', 'bold')
+    const dim2 = doc.getTextDimensions(
+        ('document').repeat(100),
+        {
+            align: 'center',
+            baseline: 'top',
+            maxWidth: w - 4 * inchToPx,
+        }
+    )
+    doc.text(
+        ('document').repeat(100),
+        w * 0.5, 10 + dim.h,
+        {
+            align: 'center',
+            baseline: 'top',
+            maxWidth: w - 4 * inchToPx,
+        }
+    )
+
+    console.log(dim.h, dim2.h)
+
+    autoTable(doc, {
+        startY: 10 + dim.h + dim2.h,
+        head: [['Name', 'Name2']],
+        body: [
+            ['Test', 'Test2'],
+            ['Test3', 'Test4'],
+        ],
+        styles: { fontSize: 14 * ptToUnit },
+        margin: { left: 5 * inchToPx, right: inchToPx },
+    })
 
     const docB = Buffer.from(doc.output('arraybuffer'))
     s.send(docB)
